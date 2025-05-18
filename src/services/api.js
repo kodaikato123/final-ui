d68dd3cd7d0e6f15f42abbe1efa49e9357a76e18
kodaikato123 <kckato.student@ua.edu.ph>
2025-05-18 12:36:09 +0800

    updated api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080'; // Update with your Spring Boot backend URL
+const API_BASE_URL = 'https://final-api-xe9w.onrender.com'; // Update with your Spring Boot backend URL
 
 export const getPosts = async () => {
   try {
